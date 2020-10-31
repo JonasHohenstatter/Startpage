0265408c482c51946dd48ab751b3f33d8148e6af
JonasHohenstatter <joni.hoh34@gmail.com>
2020-10-31 12:18:41 +0100

    Routing fix

--- home/src/views/Home.vue
+++ home/src/views/Home.vue
@@ -1,27 +1,28 @@
 <template>
     <div class="d-flex justify-content-center flex-column">
         <h3 style="margin-top: 6vh">Erstelle hier deine Startseite mit deinem Alter!</h3>
+        <h4 style="margin-top: 1vh">Gib hier dein Gebursdatum an: </h4>
         <div class='d-flex flex-row justify-content-center' style='margin-top: 3vh'>
-            <div class="dropdown" style='margin-left: 2vw; marginm-right: 2vw'>
+            <div class="dropdown">
                 <button class="btn btn-secondary dropdown-toggle"  type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Tag</button>
                 <div class="dropdown-menu" aria-labelledby="dropdownMenuButton" style="height: 25vh; overflow-y: scroll">
                     <a v-for="n in 31" class='dropdown-item' :key="n" @click="setDay(n)">{{n}}</a>
                 </div>
             </div>
-            <div class="dropdown" style='margin-left: 2vw; marginm-right: 2vw'>
+            <div class="dropdown" style='margin-left: 2vw'>
                 <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Monat</button>
                 <div class="dropdown-menu" aria-labelledby="dropdownMenuButton" style="height: 25vh; overflow-y: scroll">
                     <a class='dropdown-item' v-for="month in months" :key='month' @click="setMonth(month)">{{month}}</a>
                 </div>
             </div>
-            <div class="dropdown" style='margin-left: 2vw; marginm-right: 2vw'>
+            <div class="dropdown" style='margin-left: 2vw'>
                 <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Jahr</button>
                 <div class="dropdown-menu" aria-labelledby="dropdownMenuButton" style="height: 25vh; overflow-y: scroll">
                     <a v-for="n in years" class='dropdown-item' :key="n" @click="setYear(n)">{{n}}</a>
                 </div>
             </div>
         </div>
-        <span style='margin-top: 8vh; font-size: 3vh'>Dein Custom Link lautet: <a v-bind:href='"https://mystartpage.vercel.app/" + birthdaystring'>https://mystartpage.vercel.app/{{birthdaystring}}</a></span>
+        <span style='margin-top: 8vh; font-size: 3vh'>Dein Custom Link lautet: <a v-bind:href='"https://mystartpage.vercel.app/#/" + birthdaystring'>https://mystartpage.vercel.app/#/{{birthdaystring}}</a></span>
         <div style='margin-top: 2vh; font-size: 3vh'>Du musst diesen Link in den Einstellungen deines Browsers als Standart Startseite einstellen</div>
     </div>
 </template>
